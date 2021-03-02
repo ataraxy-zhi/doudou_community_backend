a7c04120d4ef5188960e7f3f3cd315e51f9aec40
ataraxy-zhi <2711059631@qq.com>
2021-03-02 15:13:56 +0800

    init

---
+++ src/main/java/com/example/doudou/DoudouApplication.java
@@ -0,0 +1,13 @@
+package com.example.doudou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoudouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoudouApplication.class, args);
+    }
+
+}
